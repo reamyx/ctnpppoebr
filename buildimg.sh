1c7732714c88dc7b59b82e61c8f318e736c7f322
zhixia <reamyx@126.com>
2019-05-30 12:18:40 +0800

    调整build脚本和镜像构建名称

--- buildimg.sh
+++ buildimg.sh
@@ -1,7 +1,7 @@
 #!/bin/env sh
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"; cd "$(dirname "$0")"
 
-IMGNM="pbrsrv" BASE="imginit"
+IMGNM="ctnpppoebr" BASE="imginit"
 
 #从参数或环境变量确定仓库路径
 PUSH="$1" PUSH="${PUSH:-$PSPATH}"
@@ -11,12 +11,8 @@ IMGNM="${PUSH:+$PUSH/}$IMGNM" BASEPL="${PUSH:+$PUSH/}$BASE"
 [ -n "$BASE" ] && docker pull "$BASEPL" && \
 docker tag "$BASEPL" "$BASE" || { echo "依赖镜像[ $BASE ]拉取失败,构建终止."; exit; }
 
-#构建
-docker build -t "$IMGNM" ./ && echo "构建镜像[ $IMGNM ]完成."
-
-#推送
+#构建,推送
+docker build -t "$IMGNM" ./ && echo "构建镜像[ $IMGNM ]完成." && \
 [ -n "$PUSH" ] && docker push "$IMGNM" && echo "推送镜像[ $IMGNM ]完成."
-
-
 
 exit 0
